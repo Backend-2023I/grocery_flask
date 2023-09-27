1079d62b1505cec5f85fd30a575ac17a1051eaf0
JavohirJalilov <javohirjaliloff@gmail.com>
2023-09-27 15:54:02 +0500

    add data

--- db.py
+++ db.py
@@ -1,16 +1,33 @@
 import tinydb
+from tinydb import Query
 
 class GroceryDB:
     def __init__(self):
         self.db = tinydb.TinyDB('db.json',indent=4, separators=(',', ': '))
         self.table = self.db.table('grocery')
 
-    def add(self, fruit: dict):
+    def add(self, grocery: dict):
         '''Add a fruit to the database
         
         Args:
             fruit (dict): A dictionary containing the fruit name, quantity, price and type
         '''
+        if grocery.get("name") == None:
+            return {"result": "name field not found"}
+        
+        elif grocery.get("quantity") == None:
+            return {"result": "quantity field not found"}
+        
+        elif grocery.get("price") == None:
+            return {"result": "price field not found"}
+        
+        elif grocery.get("type") == None:
+            return {"result": "type field not found"}
+        
+        else:
+            self.table.insert(grocery)
+            return {"result": "successfully"}
+
 
     def all(self):
         '''Get all fruits from the database'''
@@ -27,6 +44,9 @@ class GroceryDB:
         Returns:
             list: A list of fruits of the specified type
         '''
+        User = Query()
+        data = self.table.search(User.type == type)
+        return data
 
     def get_by_name(self, name: str) -> list:
         '''Get all fruits of a specific name from the database
@@ -37,6 +57,9 @@ class GroceryDB:
         Returns:
             list: A list of fruits of the specified name
         '''
+        User = Query()
+        data = self.table.get(User.name == name)
+        return data
 
     def get_by_price(self, price: float) -> list:
         '''Get all fruits of a specific price from the database
@@ -47,4 +70,7 @@ class GroceryDB:
         Returns:
             list: A list of fruits of the specified price
         '''
+        User = Query()
+        data = self.table.search(User.price == price)
+        return data
     
